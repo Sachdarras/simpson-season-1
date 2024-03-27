cdd2f7cae3ad72d79cf74039b6cacac231b5a9c4
Sachdarras <sachdarras@gmail.com>
2024-03-27 11:22:06 +0100

    check

---
+++ simpson-season/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Avatar from "./composants/Avatar";
+
+
+function App () {
+  
+  return (  
+  <div>
+
+    <Avatar  firstName="bart"  lastName="Simpsons" image="https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png"/>
+
+
+    <Avatar firstName="Homer" lastName="Simpsons" image="https://www.stickees.com/files/cartoon/the-simpsons/2259-garage-band-homer-sticker.png"/>
+  </div> );
+}
+
+export default App;
---
+++ simpson-season/src/composants/Avatar.jsx
@@ -0,0 +1,19 @@
+
+
+
+function Avatar(props){
+
+  return(
+<div>
+<img src={props.image}></img>
+<h1>{props.firstName} {props.lastName}</h1>
+</div>
+)
+}
+
+
+
+
+
+
+  export default Avatar
