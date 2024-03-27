2efc7216bb90be21fe1007e83977ab9de1616e50
Sachdarras <sachdarras@gmail.com>
2024-03-27 15:25:20 +0100

    full familly

--- simpson-season/src/App.jsx
+++ simpson-season/src/App.jsx
@@ -1,23 +1,55 @@
+import Avatar from "./components/Avatar";
 
-import Avatar from "./composants/Avatar";
+function App() {
+	const personnages = [
+		{
+			image: "https://www.stickees.com/files/cartoon/the-simpsons/2247-homer-simpson-beer.png",
+			firstName: "Homer",
+			lastName: "Simpson",
+			devise: "Oh Pinaise",
+		},
+		{
+			image: "https://www.stickees.com/files/cartoon/the-simpsons/2252-marge-simpson-sticker.png",
+			firstName: "Marge",
+			lastName: "Simpson",
+			devise: "Hooooomeeeer",
+		},
+		{
+			image: "https://www.stickees.com/files/cartoon/the-simpsons/2240-bart-simpson-skate.png",
+			firstName: "Bart",
+			lastName: "Simpson",
+		},
+		{
+			image: "https://www.stickees.com/files/cartoon/the-simpsons/2250-lisa-simpson-sticker.png",
+			firstName: "Lisa",
+			lastName: "Simpson",
+		},
+		{
+			image: "https://www.stickees.com/files/cartoon/the-simpsons/2251-maggie-simpson-sticker.png",
+			firstName: "Maggie",
+			lastName: "Simpson",
+		},
+		{
+			image: "https://www.simpsonspark.com/images/persos/contributions/petit-papa-noel-22647.jpg",
+			firstName: "petit Papa Noel",
+			lastName: "Simpson",
+			devise: "Wouf",
+		},
+	];
 
-
-function App () {
-  
-  return (  
-  <div>
-
-    <Avatar  firstName="bart"  lastName="Simpsons" image="https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png"/>
-
-
-    <Avatar firstName="Homer" lastName="Simpsons" image="https://www.stickees.com/files/cartoon/the-simpsons/2259-garage-band-homer-sticker.png"/>
-
-    <Avatar firstName="Marge" lastName="Simpsons" image="https://www.stickees.com/files/cartoon/the-simpsons/2252-marge-simpson-sticker.png"/>
-
-    <Avatar firstName="Lisa" lastName="Simpsons" image="https://www.stickees.com/files/cartoon/the-simpsons/2262-itunes-lisa-sticker.png"/>
-
-     <Avatar firstName="Maggy" lastName="Simpsons" image="https://www.stickees.com/files/cartoon/the-simpsons/2374-ftp-client-sticker.png"/>
-      </div> )
+	return (
+		<>
+			<h1>La famille Simpson</h1>
+			{personnages.map((personnage, index) => (
+				<Avatar
+					key={index}
+					img={personnage.image}
+					firstName={personnage.firstName}
+					lastName={personnage.lastName}
+					devise={personnage.devise}
+				/>
+			))}
+		</>
+	);
 }
-
-export default App;
+export default App
