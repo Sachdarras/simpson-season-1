00fb59201a7edbe316c2e51a86546affdf00a343
Sachdarras <sachdarras@gmail.com>
2024-03-28 17:19:40 +0100

    full simpson

--- simpson-season/src/App.jsx
+++ simpson-season/src/App.jsx
@@ -7,33 +7,40 @@ function App() {
 			firstName: "Homer",
 			lastName: "Simpson",
 			devise: "Oh Pinaise",
+			Donut: 72
 		},
 		{
 			image: "https://www.stickees.com/files/cartoon/the-simpsons/2252-marge-simpson-sticker.png",
 			firstName: "Marge",
 			lastName: "Simpson",
 			devise: "Hooooomeeeer",
+			Donut: 0
+		
 		},
 		{
 			image: "https://www.stickees.com/files/cartoon/the-simpsons/2240-bart-simpson-skate.png",
 			firstName: "Bart",
 			lastName: "Simpson",
+			Donut:15
 		},
 		{
 			image: "https://www.stickees.com/files/cartoon/the-simpsons/2250-lisa-simpson-sticker.png",
 			firstName: "Lisa",
 			lastName: "Simpson",
+			Donut:3
 		},
 		{
 			image: "https://www.stickees.com/files/cartoon/the-simpsons/2251-maggie-simpson-sticker.png",
 			firstName: "Maggie",
 			lastName: "Simpson",
+			Donut:6
 		},
 		{
 			image: "https://www.simpsonspark.com/images/persos/contributions/petit-papa-noel-22647.jpg",
 			firstName: "petit Papa Noel",
 			lastName: "Simpson",
 			devise: "Wouf",
+			
 		},
 	];
 
@@ -47,6 +54,7 @@ function App() {
 					firstName={personnage.firstName}
 					lastName={personnage.lastName}
 					devise={personnage.devise}
+					Donut={personnage.Donut}
 				/>
 			))}
 		</>
--- simpson-season/src/composants/Avatar.jsx
+++ simpson-season/src/composants/Avatar.jsx
@@ -1,13 +1,22 @@
-
+import { useState } from "react";
 function Avatar(props){
-
-  return(
-<div>
-<img src={props.img}></img>
-<h1>{props.firstName} {props.lastName}</h1>
-</div>
-)
+const [Donut , setDonut] =useState(props.Donut?props.Donut : 0)
+const increaseCount = () => {
+  setDonut(Donut +1)
 }
+  return(
+
+<div>
+
+<img src={props.img}></img>
+
+<h1>{props.firstName} {props.lastName} {props.devise}</h1>
+<button onClick={ increaseCount}>🍩{Donut} {(Donut === 0 ? "j'ai la dalle" : "J'ai bien mangé")}</button>
+</div>
+
+);
+
+  }
 
 
 
